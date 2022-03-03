a88f2d703b69d788f3a0526ee793d4ef1aadb85a
kimisak <isak7263@gmail.com>
2022-03-03 20:47:13 +0900

    setting
    start

---
+++ src/main/java/com/project/ComponentScanBasePackage.java
@@ -0,0 +1,8 @@
+package com.project;
+
+/*
+* 컴포넌트 스캔의 basePackages를 설정한다.
+* */
+public class ComponentScanBasePackage {
+
+}
--- src/main/java/com/project/blog/BlogApplication.java
+++ src/main/java/com/project/blog/BlogApplication.java
@@ -3,8 +3,22 @@ package com.project.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// 상위 패키지를 스캔의 기준으로 한다.
+import com.project.ComponentScanBasePackage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Configuration, @EnableAutoConfiguration, @ComponentScan을 지정한 것과 동일
+// 컴포넌트 구성과 자동 구성
+@SpringBootApplication(scanBasePackageClasses = {ComponentScanBasePackage.class})
+
+@RestController
 public class BlogApplication {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
