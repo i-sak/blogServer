97c876f490225101b94a2b256dec8fea3d0b4344
isaac <isaac7263@naver.com>
2022-03-16 13:32:21 +0900

    UserServiceTests

---
+++ src/test/java/com/project/blog/sservice/UserServiceTests.java
@@ -0,0 +1,21 @@
+package com.project.blog.sservice;
+
+import com.project.blog.model.UserEntity;
+import com.project.blog.service.UserService;
+import org.apiguardian.api.API;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.AssertionErrors;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*; // assertXxx
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTests {
+
+}
