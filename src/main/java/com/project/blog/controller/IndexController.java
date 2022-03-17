8f512f1db6aa1ed8661cb5c33c9fad7cc7774d6f
kimisak <isak7263@gmail.com>
2022-03-17 23:40:49 +0900

    mockMVC test (failed)

--- src/main/java/com/project/blog/controller/IndexController.java
+++ src/main/java/com/project/blog/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.project.blog.controller;
 
 import com.project.blog.service.SecurityService;
+import com.project.blog.service.SecurityServiceImpl;
 import com.project.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,9 @@ import java.util.Map;
 @RequestMapping("/")
 public class IndexController {  // 요청을 제일 먼저 맞이하는 것이 Controller
 
-    @Autowired
-    private SecurityService securityService;
+    @Autowired(required = true)
+    private SecurityServiceImpl securityService;
     //private UserService userService;
-
-
 
     //@RequestMapping(value="/", method = RequestMethod.GET)
     //@ResponseBody
--- src/test/java/com/project/blog/controller/UserStudyMockMVCTests.java
+++ src/test/java/com/project/blog/controller/UserStudyMockMVCTests.java
@@ -6,14 +6,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@ExtendWith(MockitoExtension.class), @ExtendWith(SpringExtension.class) 모두 가능
 @ExtendWith(SpringExtension.class)
@@ -21,18 +24,43 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserStudyMockMVCTests {
 
     @Autowired
-    private WebApplicationContext ctx;
-    @Autowired
     private MockMvc mockMvc;
 
-    @Before(value = "")
+    @Test
+    public void testUserIntroduction() throws Exception {
+        String content = "test";
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/users/100")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isOk())
+                .andExpect(content().string("안녕하세요."))
+                .andDo(print());
+    }
+
+    /*
+    강의용 예제
+
+    @Autowired
+    private WebApplicationContext ctx;
+    private MockMvc mockMvc;
+
+    @Before("")
     public void setUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.ctx).build();
     }
 
     @Test
     public void testBasicMVC() throws Exception {
-
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("result", status().is(200)))
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        System.out.println("{testBasicMVC} response : " + content);
     }
+    */
 
 }
+
