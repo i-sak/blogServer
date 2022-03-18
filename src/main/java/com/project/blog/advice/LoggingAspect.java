047fe345e387a4c3fba56f5c5287d19b3f3c8ffd
kimisak <isak7263@gmail.com>
2022-03-18 23:37:30 +0900

    Spring Boot starter AOP setting - upgrade

--- src/main/java/com/project/blog/advice/LoggingAspect.java
+++ src/main/java/com/project/blog/advice/LoggingAspect.java
@@ -4,11 +4,27 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect     // 컴포넌트와 함께 해서 Aspect로 setting
 @Component  // server 로드 시 컴포넌트가 같이 세팅되어 적용된다.
 public class LoggingAspect {
+
+    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    // UserService의 모든 메서드
+    @Around("execution (* com.project.blog.service.UserService.*(..))")
+    public Object logging(ProceedingJoinPoint pjp) throws Throwable {
+        logger.info("start - " + pjp.getSignature().getDeclaringTypeName()
+        + " / " + pjp.getSignature().getName());
+        Object result = pjp.proceed();
+        logger.info("finished - " + pjp.getSignature().getDeclaringTypeName()
+        + " / " + pjp.getSignature().getName());
+        return result;
+    }
+
 
     // point-cut 문법 : spring-aop 에선 메소드 단위의 위치를 지정한다.
     @Around("execution(* com.project.blog.controller.UserStudyController.*(..))")
